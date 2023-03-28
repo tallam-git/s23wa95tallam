dd0726135918d5494c2d6fed81330125ccc583bf
tallam-git <tallamcharanteja99@gmail.com>
2023-03-28 13:19:38 -0500

    Updated user.js with sum and more

--- routes/users.js
+++ routes/users.js
@@ -2,8 +2,13 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
+var sum=0;
+var more=0;
+
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  more = more + 1;
+  sum = sum + more;
+  res.send(`Sum is ${sum}`);
 });
 
 module.exports = router;
